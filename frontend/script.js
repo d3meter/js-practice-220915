9a68dbdfa8349aa722ecd674db3531d90ba1857a
d3meter <mark.demeter3@gmail.com>
2022-09-15 11:18:56 +0200

    change script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,11 @@
 function loadEvent() {
-    const newCatbutton = document.querySelector(".add-cat");
-    newCatbutton.addEventListener("click",function () {
+    const newCatButton = document.querySelector(".add-cat");
+    newCatButton.addEventListener("click", function () {
         const catContainer = document.querySelector(".cats");
-        const newCat = '<img src="https://cataas.com/cat" alt="">';
-        catContainer.innerHTML+-newCat;
-})
+        const now = new Date().getTime();
+        const newCat = '<img src="https://cataas.com/cat?random=${now}" alt="" />';
+        catContainer.insertAdjacentHTML("beforeend", newCat);
+    });
 }
 
 window.addEventListener("load", loadEvent);
